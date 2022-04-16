d389ea658af65f9ff9a26135cadd646ee9c71546
rropetsku <roope.rantalaiho@edu.turkuamk.fi>
2022-04-16 16:51:19 +0300

    Lisätty päätiedosto hirsipuuta varten, sekä tarvittavat metodit tyhjinä

---
+++ Hirsipuu.java
@@ -0,0 +1,30 @@
+
+
+public class Hirsipuu(){
+    //Käynnistää paaValikko-metodin
+    public static void main(String args[]){
+    }
+    //Antaa käyttäjän valita yksinPeli- ja pelaajaVsKone-metodin, ohjeiden ja lopetuksen välillä
+    public static void paaValikko(){
+    }
+    //Pelaaja pelaa yksin hirsipuuta
+    public static void yksinPeli(){
+    }
+    //Pelaaja ja kone arvaavat vuorotellen
+    public static void pelaajaVsKone(){
+    }
+    //Hakee tiedostosta arvattavan sanan ja palauttaa sen
+    public static String arvattavaSana(){
+    }
+    //Tulostaa annetun luvun mukaan hirsipuun tilan
+    public static void hirsipuunTulostus(int hirsipuunTila){
+    }
+    //Palauttaa boolean arvon sen mukaan onko arvattuKirjain sanassa
+    public static boolean onkoKirjainSanassa(String sana,char arvattuKirjain){
+    }
+    //Vaihtaa sanaTulosteeseen arvattuKirjain-arvon indekseihin, jossa sana-merkkijonossa löytyy kyseinen merkki 
+    public static paljastaKirjaimet(String sana,String sanaTuloste,char arvattuKirjain){
+    }
+
+}
+
