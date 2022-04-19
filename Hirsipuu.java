68fd0c3f2428623051832b52dd66f0c24bb3c595
NanSal <nanna.salmela@edu.turkuamk.fi>
2022-04-19 11:59:40 +0300

    Lisätty scanner ja yksinPelin alku

--- Hirsipuu.java
+++ Hirsipuu.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 
-
-public class Hirsipuu(){
+public class Hirsipuu {
+	public static final Scanner lukija = new Scanner(System.in);
+	
     //Käynnistää paaValikko-metodin
     public static void main(String args[]){
     }
@@ -9,6 +11,22 @@ public class Hirsipuu(){
     }
     //Pelaaja pelaa yksin hirsipuuta
     public static void yksinPeli(){
+    	String sana = arvattavaSana();
+    	String sanaTuloste;
+    	int kierrosLaskuri = 0;
+    	int hirsipuunTila = 0;
+    	
+    	while(hirsipuunTila <= 8) {
+    		hirsipuunTulostus(hirsipuunTila);
+    		for (int i=0; i<sana.length(); i++) {
+    			sanaTuloste += "*";
+    		}
+    		kierrosLaskuri += 1;
+    		System.out.println(sanaTuloste);
+    		System.out.println("Arvaa kirjain: ");
+    		String arvattuKirjain = lukija.nextLine();
+    	}
+
     }
     //Pelaaja ja kone arvaavat vuorotellen
     public static void pelaajaVsKone(){
